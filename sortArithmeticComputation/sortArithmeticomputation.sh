a0fe371b8efb2356d9a1359ccd4f8200a7913f66
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-09-23 13:39:27 +0530

    compute a*b+c

--- sortArithmeticComputation/sortArithmeticomputation.sh
+++ sortArithmeticComputation/sortArithmeticomputation.sh
@@ -4,4 +4,6 @@ echo "This problem computes different arithmetic expressions and Sorts the resul
 
 read -p "enter three numbers" a b c
 
-compute=`awk 'BEGIN{printf("%0.2f", '$a' + '$b' * '$c')}'`
+compute1=`awk 'BEGIN{printf("%0.2f", '$a' + '$b' * '$c')}'`
+
+compute2=`awk 'BEGIN{printf("%0.2f", '$a' * '$b' + '$c')}'`
