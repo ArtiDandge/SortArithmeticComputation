6022adc3aa2e299d5f89d37bb2ff7295ca59f63e
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-09-23 14:20:31 +0530

    store result of every computation in dictionary

--- sortArithmeticComputation/sortArithmeticomputation.sh
+++ sortArithmeticComputation/sortArithmeticomputation.sh
@@ -2,13 +2,16 @@
 
 echo "This problem computes different arithmetic expressions and Sorts the results"
 
-read -p "enter three numbers" a b c
+declare -A resultDict
+read -p "enter three numbers " a b c
 
-compute1=`awk 'BEGIN{printf("%0.2f", '$a' + '$b' * '$c')}'`
+resultDict["1"]=`awk 'BEGIN{printf("%0.2f", '$a' + '$b' * '$c')}'`;
 
-compute2=`awk 'BEGIN{printf("%0.2f", '$a' * '$b' + '$c')}'`
+resultDict["2"]=`awk 'BEGIN{printf("%0.2f", '$a' * '$b' + '$c')}'`;
 
-compute3=`awk 'BEGIN{printf("%0.2f", '$c' + '$a' / '$b')}'`
+resultDict["3"]=`awk 'BEGIN{printf("%0.2f", '$c' + '$a' / '$b')}'`;
 
-compute4=`awk 'BEGIN{printf("%0.2f", '$a' % '$b' + '$c')}'`
+resultDict["4"]=`awk 'BEGIN{printf("%0.2f", '$a' % '$b' + '$c')}'`;
 
+echo ${!resultDict[@]}
+echo ${resultDict[@]}
